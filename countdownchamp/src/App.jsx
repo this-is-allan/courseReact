b6c0f5911a926870f3b68a67ff41235c44f512e8
Allan Carlos <allancarlosdeveloper@gmail.com>
2017-12-25 20:12:23 -0300

    Changing data  with the input field

--- countdownchamp/src/App.jsx
+++ countdownchamp/src/App.jsx
@@ -2,10 +2,24 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            deadline: 'December 25, 2017'
+        }
+    }
+
+    changeDeadLine() {
+        console.log('state', this.state);
+        this.setState({deadline: this.state.newDeadLine})
+    }
+    
     render() {
         return (
             <div className="App">
-                <div className="App-title">Countdown to December 25, 2017</div>
+                <div className="App-title">
+                    Countdown to {this.state.deadline}
+                </div>
                 <div>
                     <div className="Clock-days">14 days</div>
                     <div className="Clock-hours">30 hours</div>
@@ -14,8 +28,13 @@ class App extends Component {
                 </div>
 
                 <div>
-                    <input placeholder='new date'/>
-                    <button>Submit</button>
+                    <input
+                        placeholder='new date'
+                        onChange={event => this.setState({newDeadLine: event.target.value})}
+                    />
+                    <button onClick={() => this.changeDeadLine()}>
+                        Submit
+                    </button>
                 </div>
             </div>
         )
