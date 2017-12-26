edf2a7c1ec3bb21567acd4294fde1c27360785ba
Allan Carlos <allancarlosdeveloper@gmail.com>
2017-12-26 11:59:50 -0300

    providing a Store and introducing Reducers

--- reminderpro/src/index.js
+++ reminderpro/src/index.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
+
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-    <App />, document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 )
