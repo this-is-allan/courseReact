05921906c63e76c95e17976d1a43ce1258d5e4b9
Allan Carlos <allancarlosdeveloper@gmail.com>
2017-12-25 21:07:42 -0300

    Adding helper methods with Ternary Expressions

--- countdownchamp/src/Clock.jsx
+++ countdownchamp/src/Clock.jsx
@@ -19,6 +19,15 @@ class Clock extends Component {
     componentDidMount() {
          setInterval(() => this.getTimeUntil(this.props.deadline), 1000)
     }
+
+    leading0(num) {
+        return num < 10 ? '0' + num : num;
+
+        // if (num < 10) {
+        //     return '0' + num;
+        // }
+        // return num;
+    }
     
     getTimeUntil(deadline) {
         const time = Date.parse(deadline) - Date.parse(new Date());
@@ -33,10 +42,10 @@ class Clock extends Component {
     render() {
         return (
             <div>
-                <div className="Clock-days">{this.state.days} days</div>
-                <div className="Clock-hours">{this.state.hours} hours</div>
-                <div className="Clock-minutes">{this.state.minutes} minutes</div>
-                <div className="Clock-seconds">{this.state.seconds} seconds</div>
+                <div className="Clock-days">{this.leading0(this.state.days)} days</div>
+                <div className="Clock-hours">{this.leading0(this.state.hours)} hours</div>
+                <div className="Clock-minutes">{this.leading0(this.state.minutes)} minutes</div>
+                <div className="Clock-seconds">{this.leading0(this.state.seconds)} seconds</div>
             </div>
         )
     }
