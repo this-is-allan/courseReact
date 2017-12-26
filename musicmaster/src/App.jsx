2806c1a086f68871c70c2962d137565c82e8da7e
Allan Carlos <allancarlosdeveloper@gmail.com>
2017-12-25 22:29:38 -0300

    preparing a web request

--- musicmaster/src/App.jsx
+++ musicmaster/src/App.jsx
@@ -6,12 +6,15 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            query: ''
+            query: 'Frank Sinatra'
         }
     }
 
     search() {
         console.log('this.state', this.state);
+        const BASE_URL = 'https://api.spotify.com/v1/search?';
+        const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+        console.log('FETCH_URL', FETCH_URL);
     }
     
     render() {
