26fd6f6f81cb5dd5b6f0b6dd03bd02e4959ae671
Allan Carlos <allancarlosdeveloper@gmail.com>
2017-12-26 21:18:56 -0300

    Adding firebase to the application

---
+++ goalcoach/src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyA780CfpxpR6bQcSnwUukP5L4BXsc1WamM",
+    authDomain: "goalcoach-756c8.firebaseapp.com",
+    databaseURL: "https://goalcoach-756c8.firebaseio.com",
+    projectId: "goalcoach-756c8",
+    storageBucket: "",
+    messagingSenderId: "322998445000"
+};
+
+export const firebaseApp = firebase.initializeApp(config);
--- goalcoach/src/index.js
+++ goalcoach/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
+import { firebaseApp } from './firebase';
 
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
+
+firebaseApp.auth().onAuthStateChanged(user => {
+    if (user) {
+        console.log('user has signed in or up', user);
+    } else {
+        console.log('user has signed out or still needs to sign in.');
+    }
+})
 
 ReactDOM.render(
     <Router path="/" history={browserHistory}>
